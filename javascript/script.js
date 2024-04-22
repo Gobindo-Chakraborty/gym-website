d3a541451e0860aaf7561c0480fa3c198e388331
Gobindo Chakraborty <gobindochakraborty108@gmail.com>
2024-04-22 09:58:26 +0600

    First draft of the design

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,12 @@
 'use strict';
 
+// Responsive menu-bar
+
+function menuClick() {
+    let x = document.getElementsByClassName("nav-items")[0];
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
